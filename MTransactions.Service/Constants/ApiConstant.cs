fee5bbd5a59f385d7604ff8dd503a22c591a60ad
Alimkhan <olimxonnutfullaevich@gmail.com>
2023-01-25 17:19:29 +0500

    Services

---
+++ MTransactions.Service/Constants/ApiConstant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTransactions.Service.Constants
+{
+    public class ApiConstant
+    {
+            public const string BASE_URL = "https://www.nbrb.by/Services/XmlExRates.aspx?ondate=";
+    }
+}
